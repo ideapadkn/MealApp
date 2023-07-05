19d3e5e8e526761d179d3da8e86e55ecc0050fe5
Kamron or Камрон <kamrush6429@gmail.com>
2023-07-05 11:46:40 +0500

    add search input to ingredients

--- src/views/Ingredients.vue
+++ src/views/Ingredients.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="p-8">
     <h1 class="text-5xl font-bold mb-4">Ingredients</h1>
+    <input 
+      type="text" 
+      class="rounded border-2 border-gray-200 w-full mb-3" 
+      placeholder="Search for Ingredients"
+      v-model="keyword"
+      @change="searchMeals"
+    >
     <router-link 
       :to="{
         name: 'byIngredients', 
         params: { ingredients: ingredient.strIngredient }
       }" 
-      v-for="ingredient of ingredients" 
+      v-for="ingredient of computedIngredients" 
       :key="ingredient.idIngredient" 
       class="block rounded p-3 mb-5 shadow"
     >
@@ -17,10 +24,18 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 import axiosClient from '../axiosClient';
 
 const ingredients = ref([])
+const keyword = ref('')
+const computedIngredients = computed(() => {
+  if(!computedIngredients) return ingredients
+  return ingredients.value.filter(i => 
+    (i.strDescription || '').toLowerCase().includes(keyword.value.toLowerCase()) ||
+    i.strIngredient.toLowerCase().includes(keyword.value.toLowerCase())
+  )
+})
 
 onMounted(() => {
   axiosClient.get('list.php?i=list')
--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -23,7 +23,12 @@ const keyword = ref('')
 const meals = computed(() => store.state.searchedMeals)
 
 function searchMeals() {
-  store.dispatch('searchMeals', keyword.value)
+  if(keyword.value) {
+    store.dispatch('searchMeals', keyword.value)
+  } else {
+    store.commit('setSearchedMeals', [])
+  }
+  
 }
 onMounted(() => {
   keyword.value = route.params.name
